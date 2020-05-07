562b9786b25d407e6c68c971ea6b5f7dee3fe7f1
Ungouge <zlobik89@gmail.com>
2020-05-07 23:41:21 +0200

    Dodano TeraZobaczymy.

---
+++ GITTest/TeraZobaczymy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITTest
+{
+    public static class TeraZobaczymy
+    {
+        const string tekst = "To nie są ćwiczenia";
+
+        public static string Tekst
+        {
+            get
+            {
+                return tekst;
+            }
+        }
+
+    }
+}
